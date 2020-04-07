a2644a8cdaaf24ecd80026adde588c5d6abbf03b
kenneth051 <kjdumba@gmail.com>
2020-04-07 23:59:48 +0200

    add display class

---
+++ src/display/__tests__/display.tests.js
@@ -0,0 +1,53 @@
+import { Display } from "../display";
+import { FakeBoardDisplay, FakeToast } from "../../fakes/display";
+
+describe("Display", () => {
+  var util;
+  beforeEach(() => {
+    util = new Display();
+  });
+  it("should set the board  if array passed in has elements", () => {
+    const board = new FakeBoardDisplay();
+    util.displayBoardMoves(["x", "o"], board.setBoard);
+    expect(board.setBoardIsCalled()).toBe(true);
+  });
+  it("should not set the board  if array passed has no elements", () => {
+    const board = new FakeBoardDisplay();
+    util.displayBoardMoves(false, board.setBoard);
+    expect(board.setBoardIsCalled()).toBe(false);
+  });
+  it("should call success toast if we have a winning message", () => {
+    const toast = new FakeToast();
+    const response = { win: true };
+    util.displayWinMessage(response, toast);
+    expect(toast.successIsCalled()).toBe(true);
+  });
+  it("should not call success toast if we have no winning message", () => {
+    const toast = new FakeToast();
+    const response = {};
+    util.displayWinMessage(response, toast);
+    expect(toast.successIsCalled()).toBe(false);
+  });
+  it("should call info toast if we have a draw message", () => {
+    const toast = new FakeToast();
+    const response = { draw: true };
+    util.displayDrawMessage(response, toast);
+    expect(toast.infoIsCalled()).toBe(true);
+  });
+  it("should not call info toast if we have no draw message", () => {
+    const toast = new FakeToast();
+    const response = {};
+    util.displayDrawMessage(response, toast);
+    expect(toast.infoIsCalled()).toBe(false);
+  });
+  it("should call error toast if we have an error message", () => {
+    const toast = new FakeToast();
+    util.displayErrors(["error"], toast);
+    expect(toast.errorIsCalled()).toBe(true);
+  });
+  it("should not call error toast if we have no error message", () => {
+    const toast = new FakeToast();
+    util.displayErrors([], toast);
+    expect(toast.errorIsCalled()).toBe(false);
+  });
+});
